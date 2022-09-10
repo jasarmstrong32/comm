0aa1d70f17cc9f217ba72596611146b82389f216
jasarmstrong32 <97754601+jasarmstrong32@users.noreply.github.com>
2022-09-10 15:02:10 -0500

    2 files modified

--- main.js
+++ main.js
@@ -1,71 +1,91 @@
+let comments = [];
+
+const addComment = (ev)=>{
+  ev.preventDefault();
+  let comment = {
+    name: document.getElementById('#name').value,
+    date: Date.now(),
+    email: document.getElementById('#email').value,
+    comm: document.getElementById('#comment-body').value
+  }
+  comments.push(comment);
+  document.forms[0].reset();
+
+  console.warn('added', {comments} );
+  let commentDisplay = document.querySelector('.comment-display');
+  commentDisplay.textContent = '\n' + JSON.stringify(comments, '\t', 2);
+}
+
+// const commentDisplay = document.querySelector('.comment-display');
+// function postComment() {
+//   commentDisplay.innerHTML = `<comment-comp></comment-comp>`
+// }
+// const button = document.getElementById('btn');
+// button.addEventListener('click', function (evt) {
+//   evt.preventDefault(); //keeps page from loading error message after submit
+//   postComment(); //function call for alerts
+//   window.location.reload(true); //reloads window after submit
+// });
+
 // run `node index.js` in the terminal
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+// console.log(`Hello Node.js v${process.versions.node}!`);
 
-const commentName = document.getElementById('#name').value;
-const commentEmail = document.getElementById('#email').value;
-const commentBody = document.getElementById('#comment-body').value;
-const date = new Date();
+// const commentName = document.getElementById('#name').value;
+// const commentEmail = document.getElementById('#email').value;
+// const commentBody = document.getElementById('#comment-body').value;
+// const date = new Date();
 
-const commentTemplate = document.createElement('template');
-commentTemplate.innerHTML = `
-  <style>
-  .comment-comp {
-    text-align: center;
-    background: #e5cf00;
-    width: 100%;
-    display: grid;
-    grid-template-columns: 1fr 2fr;
-    grid-gap: 10px;
-    margin-bottom: 15px;
-    border-bottom: #540707 5px solid;
-  }
-  p[class='comment-body'] {
-    color: #540707;
-    font-size: 1.5rem;
-  }
-  div[class='comment-info']{
-    color: #540707;
-    font-size: 1rem;
-  }
-  </style>
-  <div class='comment-comp'>
-      <p class='comment-body'></P>
-      <div class='comment-info'>
-        <p class='comment-name'></p>
-        <p class='comment-email'></P>
-        <p class='comment-dateTime'></p>
-    </div>
-`;
+// const commentTemplate = document.createElement('template');
+// commentTemplate.innerHTML = `
+//   <style>
+//   .comment-comp {
+//     text-align: center;
+//     background: #e5cf00;
+//     width: 100%;
+//     display: grid;
+//     grid-template-columns: 1fr 2fr;
+//     grid-gap: 10px;
+//     margin-bottom: 15px;
+//     border-bottom: #540707 5px solid;
+//   }
+//   p[class='comment-body'] {
+//     color: #540707;
+//     font-size: 1.5rem;
+//   }
+//   div[class='comment-info']{
+//     color: #540707;
+//     font-size: 1rem;
+//   }
+//   </style>
+//   <div class='comment-comp'>
+//       <p class='comment-body'></P>
+//       <div class='comment-info'>
+//         <p class='comment-name'></p>
+//         <p class='comment-email'></P>
+//         <p class='comment-dateTime'></p>
+//     </div>
+// `;
 
-class CommentComp extends HTMLElement {
-  constructor() {
-    super();
+// class CommentComp extends HTMLElement {
+//   constructor() {
+//     super();
 
-    this.attachShadow({ mode: 'open' });
-    this.shadowRoot.appendChild(commentTemplate.content.cloneNode(true));
+//     this.attachShadow({ mode: 'open' });
+//     this.shadowRoot.appendChild(commentTemplate.content.cloneNode(true));
 
-    this.shadowRoot.querySelector('p[class="comment-body"]').innerText =
-      `${commentBody}`;
+//     this.shadowRoot.querySelector('p[class="comment-body"]').innerText =
+//       `${commentBody}`;
 
-    this.shadowRoot.querySelector('p[class="name"]').innerText = `${commentName}`;
+//     this.shadowRoot.querySelector('p[class="name"]').innerText = `${commentName}`;
 
-    this.shadowRoot.querySelector('p[class="email"]').innerText = `${commentEmail}`;
+//     this.shadowRoot.querySelector('p[class="email"]').innerText = `${commentEmail}`;
 
-    this.shadowRoot.querySelector('p[class="comment-dateTime"]').innerTeaxt = `${date}`
-  }
-}
+//     this.shadowRoot.querySelector('p[class="comment-dateTime"]').innerTeaxt = `${date}`
+//   }
+// }
 
-window.customElements.define('comment-comp', CommentComp);
+// window.customElements.define('comment-comp', CommentComp);
 
-const commentDisplay = document.querySelector('.comment-display');
 
-function postComment() {
-  commentDisplay.innerHTML = `<comment-comp></comment-comp>`
-}
-const button = document.getElementById('submit');
-button.addEventListener('click', function (evt) {
-  evt.preventDefault(); //keeps page from loading error message after submit
-  postComment(); //function call for alerts
-  window.location.reload(true); //reloads window after submit
-});
+
